ec086eb76273499a0501aad0c6610a7a48b89039
João Duarte Colares <95177870+joao314159@users.noreply.github.com>
2024-03-11 08:54:48 -0300

    Update principal.cpp

--- principal.cpp
+++ principal.cpp
@@ -4,23 +4,29 @@
 using namespace std;
 
 class Grafo1{
+    
+    public:
 
-    Grafo(){}
-    Grafo(int N){
+    Grafo1(){}
+    
+    Grafo1(int N){
         this->N = N;
     }
-    Grafo(int N,unsigned* pesos, unsigned* nos_pretos, unsigned* nos_coloridos){
+    
+    Grafo1(int N, unsigned* nos_pretos, unsigned* nos_coloridos){
+        
         this->N = N;
 
         for(int i =0;i<N;i++){
-
+            
+            this->pesos.push_back(1);
+            
             this->nos_pretos.push_back(nos_pretos[i]);
 
             this->nos_coloridos.push_back(nos_coloridos[i]);
         }
         this->nos_pretos.push_back(0);
         this->nos_coloridos.push_back(0);
-
 
     }
 
@@ -35,6 +41,7 @@ class Grafo1{
     vector<unsigned> ordenar(){
 
         vector<unsigned> iniciais;
+        vector<unsigned> resultado;
 
         unsigned* lista = new unsigned[N];
 
@@ -42,47 +49,48 @@ class Grafo1{
             lista[N] = 0;
         }
 
-        for(int i= 0;i<N;i++){
-
+        for(int i = 0;i < N;i++){
+            //se há mais de uma aresta chegando no vértice nos_pretos[i], nos_pretos[i] é aumentado em 1
             if(nos_pretos[i]){
                 lista[nos_pretos[i]]++;
             }
+            //se há mais de uma aresta chegando no vértice nos_coloridos[i], nos_coloridos[i] é aumentado em 1
             if(nos_coloridos[i]){
                 lista[nos_coloridos[i]]++;
             }
         }
 
         for(int i = 0;i<N;i++){
-            if(lista[i] > 0){
+            if(lista[i] == 0){
                 iniciais.push_back(i);
+                cout<<"vértice inicial: "<<i<<endl;
             }
-
         }
-
+        
+        //coloca os elementos iniciais no vetor de retorno
+        for(int i = 0;i<iniciais.size();i++){
+            cout<<i<<endl;
+            resultado.push_back(iniciais[i]);
+        }
+        
+        for(int i = 0;i<iniciais.size();i++){
+            cout<<i<<endl;
+        }
+        
+        return resultado;
     }
-
-
 };
-
-
-
-
-
-
-
-
-
-
-
 
 
 int main()
 {
-
-
-
-
-
-    cout << "Hello world!" << endl;
+    
+    unsigned nos_pretos[6] = {0,6,5,2,1,0};
+    unsigned nos_coloridos[6] = {0,1,0,5,0,0};
+    
+    Grafo1 grafo(6,nos_pretos,nos_coloridos);
+    
+    grafo.ordenar();
+    
     return 0;
 }
